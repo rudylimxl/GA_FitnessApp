685cb75284ec6d6265fffd030632c64c931fddfc
rudylimxl <rudylimxl@gmail.com>
2023-09-12 19:06:34 +0800

    model for user and userDetail

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,25 @@ import mongoose from "mongoose";
 
 // TO EDIT
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Your email address is required"],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Your username is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Your password is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 
