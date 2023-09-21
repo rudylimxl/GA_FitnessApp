abb025c5c3cf9228ad865737052932ab8d71fae0
Keane <bkeaneneoweizhe@gmail.com>
2023-09-21 11:56:08 +0800

    fix: update user routes in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
-import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 import "dotenv/config";
-import mongoose from "./src/config/mongoDB.js";
-import bodyParser from "body-parser";
-import userRouter from "./src/routes/userRouter.js";
+import express from "express";
 import postRouter from "./src/routes/postRouter.js";
+import userRouter from "./src/routes/userRouter.js";
 
 const app = express();
 
@@ -15,7 +14,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 //Routes
-app.use("/", userRouter);
+app.use("/users", userRouter);
 app.use("/posts", postRouter);
 
 app.listen(process.env.PORT, () => {
