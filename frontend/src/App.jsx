f78ce567eccc27b6b2748f5fd415935d7bebdeb3
Keane <bkeaneneoweizhe@gmail.com>
2023-09-21 09:55:21 +0800

    basic mui template upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Test from "./components/test/test.jsx";
-import UserProfile from "./components/test/User.jsx";
-import Posts from "./components/test/Posts.jsx";
-import CreatePost from "./components/test/CreatePost.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Profile from "./pages/Profile.jsx";
+import CreatePost from "./components/test/CreatePost.jsx";
+import Posts from "./components/test/Posts.jsx";
+import UserProfile from "./components/test/User.jsx";
+import Test from "./components/test/test.jsx";
 import AppHome from "./pages/AppHome.jsx";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile.jsx";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/app" element={<AppHome />} />
-
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/user" element={<Profile />}>
             <Route index element={<UserProfile />} />
             <Route path="posts" element={<Posts />} />
