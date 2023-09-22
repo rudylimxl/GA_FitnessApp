2ca39a0137fb2f126f78e949dfce2a5a15791a4f
Benjamin Mak <121108280+benjamin-mak@users.noreply.github.com>
2023-09-22 14:51:58 +0800

    Added effect to get profile info from DB

--- frontend/src/components/edit-profile/Profile.jsx
+++ frontend/src/components/edit-profile/Profile.jsx
@@ -2,41 +2,67 @@ import Avatar from "@mui/material/Avatar";
 import styles from "./Profile.module.css";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 import EditProfile from "./EditProfile";
 import ProfileInfo from "./ProfileInfo";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Profile = () => {
-  return (
-    <div className={styles.grid}>
-      <Avatar
-        alt="Profile picture"
-        src="/static/images/avatar/1.jpg"
-        sx={{ width: 100, height: 100, marginTop: "20px" }}
-      />
-      <div className={styles.profileInfo}>
-        <ProfileInfo />
+  const [info, setInfo] = useState(null);
+
+  useEffect(() => {
+    const userId = "6505b4f0940b11b3fe8a55d7";
+
+    //Gets profile info from database
+    const getInfo = async () => {
+      const res = await axios.get(`http://localhost:8000/users/${userId}`);
+      setInfo(res.data);
+    };
+
+    getInfo();
+  }, []);
+
+  if (info === null) {
+    return (
+      <Box sx={{ display: "flex", margin: "50px auto", width: "10%" }}>
+        <CircularProgress />
+      </Box>
+    );
+  } else {
+    return (
+      <div className={styles.grid}>
+        <Avatar
+          alt="Profile picture"
+          src={info.avatarUrl}
+          sx={{ width: 100, height: 100, marginTop: "20px" }}
+        />
+        <div className={styles.profileInfo}>
+          <ProfileInfo info={info} />
+        </div>
+        <div className={styles.buttonGrp}>
+          <EditProfile />
+          <Button
+            variant="contained"
+            size="small"
+            startIcon={<AddIcon />}
+            sx={{ width: "150px" }}
+          >
+            Log workout
+          </Button>
+          <Button
+            variant="contained"
+            size="small"
+            startIcon={<AddIcon />}
+            sx={{ width: "150px" }}
+          >
+            Post video
+          </Button>
+        </div>
       </div>
-      <div className={styles.buttonGrp}>
-        <EditProfile />
-        <Button
-          variant="contained"
-          size="small"
-          startIcon={<AddIcon />}
-          sx={{ width: "150px" }}
-        >
-          Log workout
-        </Button>
-        <Button
-          variant="contained"
-          size="small"
-          startIcon={<AddIcon />}
-          sx={{ width: "150px" }}
-        >
-          Post video
-        </Button>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Profile;
